facc000e2e6829a68a3673b7a7b9527825f98f97
Francisco Mendoza <jfranciscomn@gmail.com>
2016-01-20 17:56:33 -0700

    Inician las formulas

---
+++ client/layouts/root.js
@@ -0,0 +1,6 @@
+angular.module("formulas").controller("RootCtrl", ['$scope', '$meteor', function ($scope, $meteor)
+{
+  $scope.isLoggedIn = function(){
+	  return Meteor.user();
+  } 
+}]);
---
+++ client/routes.js
@@ -0,0 +1,87 @@
+angular.module("formulas").run(function ($rootScope, $state, toastr) {
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    switch(error) {
+      case "AUTH_REQUIRED":
+        $state.go('anon.login');
+        break;
+      case "FORBIDDEN":
+        //$state.go('root.home');
+        break;
+      case "UNAUTHORIZED":
+      	toastr.error("Acceso Denegado");
+				toastr.error("No tiene permiso para ver esta opción");
+        break;
+      default:
+        $state.go('internal-client-error');
+    }
+/*
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('anon.login');
+    }
+*/
+  });
+});
+
+angular.module('formulas').config(['$injector', function ($injector) {
+  var $stateProvider = $injector.get('$stateProvider');
+  var $urlRouterProvider = $injector.get('$urlRouterProvider');
+  var $locationProvider = $injector.get('$locationProvider');
+
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/');
+
+  /***************************
+   * Anonymous Routes
+   ***************************/
+  $stateProvider
+    .state('anon', {
+      url: '',
+      abstract: true,
+      template: '<ui-view/>'
+    })
+    .state('anon.login', {
+      url: '/login',
+      templateUrl: 'client/login/login.ng.html',
+      controller: 'LoginCtrl',
+      controllerAs: 'lc'
+    })
+    .state('anon.logout', {
+      url: '/logout',
+      resolve: {
+        'logout': ['$meteor', '$state', 'toastr', function ($meteor, $state, toastr) {
+          return $meteor.logout().then(
+            function () {
+	            toastr.success("Vuelva pronto.");
+              $state.go('anon.login');
+            },
+            function (error) {
+              toastr.error(error.reason);
+            }
+          );
+        }]
+      }
+    });
+
+  /***************************
+   * Login Users Routes
+   ***************************/
+  $stateProvider
+    .state('root', {
+      url: '',
+      abstract: true,
+      templateUrl: 'client/layouts/root.ng.html',
+      controller: 'RootCtrl',
+    })
+    .state('root.home', {
+      url: '/',
+      templateUrl: 'client/home/home.ng.html',      
+      resolve: {
+	      "currentUser": ["$meteor", function($meteor){
+	        return $meteor.requireUser();
+	      }]
+	    }
+    }); 
+}]);
+ 
